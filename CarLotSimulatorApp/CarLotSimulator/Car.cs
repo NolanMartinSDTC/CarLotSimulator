ff30437dca267e1d41987a58170a5856532967d2
Nolan Martin <nmartinsdtc@gmail.com>
2023-03-17 11:12:14 -0600

    exercise 2 with bonuses

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace CarLotSimulator
+{
+	public class Car
+	{
+        //Create a seperate class file called Car
+        //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+        //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+        //The methods should take one string parameter: the respective noise property
+
+		public Car()
+		{
+		}
+
+        //Custom Constructor
+        public Car(int year, string make, string model, bool driveable, string engineNoise, string honkNoise)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            IsDriveable = driveable;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+        }
+        public int Year { get; set; }
+		public string Make { get; set; }
+        public string Model { get; set; }
+        public bool IsDriveable { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+
+        public void MakeEngineNoise()
+        {
+            Console.WriteLine($"The car engine goes: {EngineNoise}");
+        }
+
+        public void MakeHonkNoise()
+        {
+            Console.WriteLine($"The car honk goes: {HonkNoise}");
+        }
+
+        
+    }
+
+    
+}
+
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CarLotSimulator
 {
@@ -15,19 +16,71 @@ namespace CarLotSimulator
 
 
             //Now that the Car class is created we can instanciate 3 new cars
-            //Set the properties for each of the cars
+
+            var parkingLot = new CarLot();
+
+            //Car mySecondCar = new Car();
+            //Car myThirdCar = new Car();
+
+            // First way to set properties for first object (Dot Notation Syntax)
+
+            Car myFirstCar = new Car();
+            myFirstCar.Year = 2001;
+            myFirstCar.Make = "Saturn";
+            myFirstCar.Model = "Ion";
+            myFirstCar.IsDriveable = false;
+            myFirstCar.EngineNoise = "bzz";
+            myFirstCar.HonkNoise = "beep";
+            parkingLot.ListOfCars.Add(myFirstCar);
+
+            // Initializing and setting properties for second object (Object Initializer Syntax)
+
+            var mySecondCar = new Car() { Year = 2012, Make = "Chevy", Model = "Cruze", IsDriveable = false, EngineNoise = "brr", HonkNoise = "baep" };
+            parkingLot.ListOfCars.Add(mySecondCar);
+
+            // Third way to initialize object and set properties (Custom Constructors)
+
+            Car myThirdCar = new Car
+                (2022,
+                "Toyota",
+                "Tacoma",
+                true,
+                "BRRRR",
+                "BAAP");
+            parkingLot.ListOfCars.Add(myThirdCar);
+
             //Call each of the methods for each car
+
+            //myFirstCar.MakeEngineNoise();
+            //myFirstCar.MakeHonkNoise();
+
+            //mySecondCar.MakeEngineNoise();
+            //mySecondCar.MakeHonkNoise();
+
+            //myThirdCar.MakeEngineNoise();
+            //myThirdCar.MakeHonkNoise();
 
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
+            // SEE ABOVE
+
+            foreach (Car car in parkingLot.ListOfCars)
+            {
+                Console.WriteLine($"Year: {car.Year} \nMake: {car.Make} \nModel: {car.Model}");
+                car.MakeEngineNoise();
+                car.MakeHonkNoise();
+            }
+
 
             //*************BONUS X 2*************//
 
             //Create a CarLot class
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
+
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
         }
     }
 }
